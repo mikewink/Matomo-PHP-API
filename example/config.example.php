252723b0fe19d83209a041e697006a95d6eb3e1e
Mike Wink <mikewink@pm.me>
2022-10-24 00:30:13 +0200

    Update config.example.php with date range const

--- example/config.example.php
+++ example/config.example.php
@@ -1,5 +1,13 @@
 <?php
 
-define('SITE_URL', '');
-define('SITE_ID', 1);
-define('TOKEN', '123456789abcdefghijklmnopqrstuvwxyz');
+const SITE_URL = '';
+const SITE_ID = 1;
+const TOKEN = '123456789abcdefghijklmnopqrstuvwxyz'; // or anonymous
+
+const RANGE_START_Y = 2022;
+const RANGE_START_M = 10;
+const RANGE_START_D = 1;
+
+const RANGE_END_Y = 2022;
+const RANGE_END_M = 10;
+const RANGE_END_D = 20;
